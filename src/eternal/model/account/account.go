1f65902b9e556bb3953aee1d837c819690f3fe16
wiiiky <wiiiky@outlook.com>
2018-05-09 23:22:53 +0800

    注销

--- src/eternal/model/account/account.go
+++ src/eternal/model/account/account.go
@@ -5,7 +5,6 @@ import (
 	"eternal/util"
 	"github.com/go-pg/pg"
 	log "github.com/sirupsen/logrus"
-	"time"
 )
 
 func GetSupportedCountries() ([]*SupportedCounty, error) {
@@ -130,22 +129,4 @@ func GetAccountWithTokenID(tokenID string) (*Account, error) {
 		return nil, err
 	}
 	return a, nil
-}
-
-func UpsertToken(userID string) (*Token, error) {
-	conn := db.Conn()
-
-	tk := &Token{
-		UserID: userID,
-		CTime:  time.Now(),
-	}
-	_, err := conn.Model(tk).
-		OnConflict("(user_id) DO UPDATE").
-		Set("id = uuid_generate_v1mc()").
-		Set("ctime = ?ctime").Insert()
-	if err != nil {
-		log.Error("UpsertToken error:", err)
-		return nil, err
-	}
-	return tk, nil
 }
