da45f4f8e3f740e409f4a920c8972f4873d9a3cf
wiiiky <wiiiky@outlook.com>
2018-05-24 11:18:36 +0800

    添加sql注释

--- doc/util.sql
+++ doc/util.sql
@@ -1,1 +1,2 @@
+-- 创建测试的热门回答
 INSERT INTO hot_answer(question_id,answer_id,topic_id) SELECT DISTINCT question.id,answer.id,question_topic.topic_id FROM question INNER JOIN answer ON answer.question_id=question.id INNER JOIN question_topic ON question_topic.question_id=question.id;
