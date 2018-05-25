884d1337ecb54fa88ff55ae366824eb3824d2c96
wiiiky <wiiiky@outlook.com>
2018-05-25 15:18:41 +0800

    更新sql

--- doc/util.sql
+++ doc/util.sql
@@ -1,2 +1,9 @@
 -- 创建测试的热门回答
-INSERT INTO hot_answer(question_id,answer_id,topic_id) SELECT DISTINCT question.id,answer.id,question_topic.topic_id FROM question INNER JOIN answer ON answer.question_id=question.id INNER JOIN question_topic ON question_topic.question_id=question.id;
+INSERT INTO hot_answer (question_id, answer_id, topic_id)
+	SELECT DISTINCT question.id, answer.id, question_topic.topic_id
+		FROM question
+			INNER JOIN answer ON answer.question_id = question.id
+			INNER JOIN question_topic ON question_topic.question_id = question.id
+				WHERE answer.id NOT IN (
+					SELECT answer_id
+						FROM hot_answer);
